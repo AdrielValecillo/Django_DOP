472dfb5d286d7f45734c5109bc50918c2b8343d9
adrielvalecillo <isaidiazvalecillo@gmail.com>
2024-05-31 13:04:20 -0500

    Creacion de llos metoods create y read

--- proyecto_django/applications/empleado/urls.py
+++ proyecto_django/applications/empleado/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = "empleado_app"
+
 urlpatterns = [
     path('listatodosempleados/', views.ListAllEmpleados.as_view()),
     path('listaempleadodepartamento/<valDpto>/', views.listByDptoEmpleado.as_view()),
     path('listadobusquedaempleado/', views.listEmpleadoByKword.as_view()),
     path('listadoempleadospaginados/', views.listAllEmpleadosPag.as_view()),
+    path('crearEmpleado/', views.CreateEmpleado.as_view()),
+    path('successempleado/', views.SuccessEmpleado.as_view(), name='proceso_correcto'),
+    path('verempleado/<pk>', views.DetailEmpleado.as_view()),
 ]
--- proyecto_django/applications/empleado/views.py
+++ proyecto_django/applications/empleado/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, TemplateView, DetailView
 from typing import Any
 from django.db.models.query import QuerySet
+from django.urls import reverse_lazy
 
 from .models import Empleado
 # Create your views here.
@@ -38,3 +39,19 @@ class listAllEmpleadosPag(ListView):
     model = Empleado
     paginate_by = 3
     ordering = 'nombreEmp'
+
+
+class CreateEmpleado(CreateView):
+    model = Empleado
+    template_name = 'empleado/add_empleado.html'
+    fields = ('__all__')
+    success_url= reverse_lazy('empleado_app:proceso_correcto')
+
+class SuccessEmpleado(TemplateView):
+    template_name = "empleado/success_empleado.html"
+
+
+class DetailEmpleado(DetailView):
+    model = Empleado
+    template_name = "empleado/detail_empleado.html"
+
